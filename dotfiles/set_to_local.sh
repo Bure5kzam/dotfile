cd5c69527cccc5fa5667eedc679062802506b4c7
Bure5kzam <73729221+Bure5kzam@users.noreply.github.com>
2023-12-10 16:39:53 +0900

    Update set_to_local.sh
    fix : dotfile 아니라도 백업하도록 수정

--- dotfiles/set_to_local.sh
+++ dotfiles/set_to_local.sh
@@ -5,6 +5,17 @@ REPO_URL=${URL_GIT_DOTFILE_REPO:-https://github.com/Bure5kzam/dotfile.git}
 
 cd $HOME
 
+# 이미 .cfg 디렉토리가 존재하는지 확인
+if [ -d "$HOME/.cfg" ]; then
+  echo ".cfg directory already exists, backing up to .config-backup"
+  
+  # 중복된 파일 백업 폴더 생성
+  mkdir -p $HOME/.config-backup
+  
+  # 기존 .cfg 디렉토리를 백업 디렉토리로 이동
+  mv $HOME/.cfg $HOME/.config-backup/.cfg_backup_$(date +%Y-%m-%d_%H-%M-%S)
+fi
+
 # dotfile 저장소 구성
 git clone --bare "$REPO_URL" $HOME/.cfg || { echo "Clone failed"; exit 1; }
 
@@ -13,19 +24,21 @@ function config {
    /usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME $@
 }
 
-# 중복된 파일 백업 폴더 생성
-mkdir -p .config-backup
-
 # dotfile 설정 적용
 config checkout
 
 # 실행했을 때 에러가 났으면 백업 프로세스 실행
 if [ $? = 0 ]; then
-  echo "Checked out config.";
+  echo "Checked out config."
 else
-  echo "Backing up pre-existing dot files.";
-  config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
-fi;
+  echo "Backing up pre-existing dot files."
+  
+  # 중복된 파일 백업 폴더 생성
+  mkdir -p $HOME/.config-backup
+  
+  # 중복된 파일을 .config-backup으로 이동
+  config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} $HOME/.config-backup/{}
+fi
 
 config checkout
 config config status.showUntrackedFiles no
