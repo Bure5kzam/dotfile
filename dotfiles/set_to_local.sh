8cb3e0c0728798a768f89432e0a1fb9936ef11a7
Bure5kzam <73729221+Bure5kzam@users.noreply.github.com>
2023-12-11 23:41:57 +0900

    Update set_to_local.sh

--- dotfiles/set_to_local.sh
+++ dotfiles/set_to_local.sh
@@ -2,7 +2,7 @@
 
 # URL_GIT_DOTFILE_REPO 변수에 저장된 값 가져오기
 REPO_URL=${URL_GIT_DOTFILE_REPO:-https://github.com/Bure5kzam/dotfile.git}
-DATE=$(date +%Y-%m-%d_%H-%M-%S)
+TODAY=$(TODAY +%Y-%m-%d_%H-%M-%S)
 cd $HOME
 
 # 이미 .cfg 디렉토리가 존재하는지 확인
@@ -13,7 +13,7 @@ if [ -d "$HOME/.cfg" ]; then
   mkdir -p $HOME/.config-backup
   
   # 기존 .cfg 디렉토리를 백업 디렉토리로 이동
-  mv $HOME/.cfg $HOME/.config-backup/.cfg_backup_$DATE
+  mv $HOME/.cfg $HOME/.config-backup/.cfg_backup_$TODAY
 fi
 
 # dotfile 저장소 구성
@@ -34,10 +34,10 @@ else
   echo "Backing up pre-existing dot files."
   BACKUP_DIR=dotfiles_backup
   # 중복된 파일 백업 폴더 생성
-  mkdir -p $HOME/$BACKUP_DIR/$DATE
+  mkdir -p $HOME/$BACKUP_DIR/$TODAY
   
   # 중복된 파일을 .config-backup으로 이동
-  config checkout 2>&1 | egrep "^\s+.*" | awk {'print $1'} | awk -F '/' {' print $1 '} | xargs -I{} mv {} $HOME/$BACKUP_DIR/$DATE/{}
+  config checkout 2>&1 | egrep "^\s+.*" | awk {'print $1'} | awk -F '/' {' print $1 '} | xargs -I{} mv {} $HOME/$BACKUP_DIR/$TODAY/{}
 fi
 
 config checkout
